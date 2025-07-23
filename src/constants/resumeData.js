6ede751096d65365fa37643fdc1eb65e546547d0
rabin20-04 <poudelrabin2004@gmail.com>
2025-07-23 09:17:48 +0545

    img update

--- src/constants/resumeData.js
+++ src/constants/resumeData.js
@@ -30,7 +30,7 @@ export const resumeData = {
       organization: "Association of Computer Engineering (ACES), IOEPC",
       description:
         "Worked with driven minds, honed leadership and teamwork skills, and gained hands-on experience in events management and mentorship at ACES.",
-      logo: "https://scontent.fbir4-1.fna.fbcdn.net/v/t39.30808-6/306272669_399023282403607_3225461323499728368_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHJyP4L6uA_GM9iFchx_McQ0HmHTVdO2ijQeYdNV07aKD9l88XuzUL8hN6KZlJCfYN7rHj6OWyJBQ0GiDa0hKhO&_nc_ohc=SbEqn2xqcqEQ7kNvwG_fHGO&_nc_oc=AdnrJXI1uvYjbj4UFmIch7YXkQqG9z-Zv029DponA4vlWC-q1MaL68DCX_bnf1_btJo&_nc_zt=23&_nc_ht=scontent.fbir4-1.fna&_nc_gid=keuvUlarz-jI5y-6K5wOSA&oh=00_AfT2IJacUbg56ldME48VooOlNT_xWjuIGzQFEKI2tKkIiw&oe=6875B2E5",
+      logo: "src/assets/306272669_399023282403607_3225461323499728368_n.jpg",
       link:"https://www.linkedin.com/company/acesioe-pc"
     },
   ],
